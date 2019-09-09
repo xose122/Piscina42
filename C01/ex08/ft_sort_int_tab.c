1ac8d8f8dc725b158e08e757e176d59e096a7755
xose122 <j.gomezbr@gmail.com>
2019-09-09 13:57:04 +0200

    Practica 01 terminada

---
+++ C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_combn.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgomez-b <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/09 10:27:53 by jgomez-b          #+#    #+#             */
+/*   Updated: 2019/09/09 10:27:58 by jgomez-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_swap(int *a, int *b)
+{
+	int aux;
+
+	aux = *a;
+	*a = *b;
+	*b = aux;
+}
+
+int		partition (int arr[], int low, int high) 
+{ 
+    int pivot;
+    int i;
+    int j;
+  
+  	i = (low - 1);
+  	j = low;
+  	pivot = arr[high];
+  	while (j <= high - 1)
+  	{
+  		if (arr[j] < pivot) 
+        { 
+            i++;
+            ft_swap(&arr[i], &arr[j]); 
+        }
+  		j++;
+  	}
+    ft_swap(&arr[i + 1], &arr[high]); 
+    return (i + 1); 
+} 
+
+void	quickSort(int arr[], int low, int high) 
+{ 
+	int pi;
+
+    if (low < high) 
+    { 
+        pi = partition(arr, low, high); 
+        quickSort(arr, low, pi - 1); 
+        quickSort(arr, pi + 1, high); 
+    } 
+}
+
+void ft_sort_int_tab(int *tab, int size)
+{
+	quickSort(tab, 0, size);
+}
