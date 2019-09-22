9b73918e475d3886edec99b954e20e5d0eed1347
Jorge Gomez <j.gomezbr@gmail.com>
2019-09-22 16:02:43 +0200

    c06/ex03 añadido

--- C06/ex02/ft_rev_params.c
+++ C06/ex02/ft_rev_params.c
@@ -13,49 +13,6 @@ void	ft_ptint_str(char *str)
 	write(1, "\n", 1);
 }
 
-void	ft_swap(int *a, int *b)
-{
-	int aux;
-
-	aux = *a;
-	*a = *b;
-	*b = aux;
-}
-
-int		partition(int arr[], int low, int high)
-{
-	int pivot;
-	int i;
-	int j;
-
-	i = (low - 1);
-	j = low;
-	pivot = arr[high];
-	while (j <= high - 1)
-	{
-		if (arr[j] < pivot)
-		{
-			i++;
-			ft_swap(&arr[i], &arr[j]);
-		}
-		j++;
-	}
-	ft_swap(&arr[i + 1], &arr[high]);
-	return (i + 1);
-}
-
-void	quick_sort(int arr[], int low, int high)
-{
-	int pi;
-
-	if (low < high)
-	{
-		pi = partition(arr, low, high);
-		quick_sort(arr, low, pi - 1);
-		quick_sort(arr, pi + 1, high);
-	}
-}
-
 int		main(int argc, char **argv)
 {
 	int i;
--- C06/ex03/ft_sort_params.c
+++ C06/ex03/ft_sort_params.c
@@ -1,2 +1,73 @@
 #include <unistd.h>
 
+void	ft_ptint_str(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &(str[i]), 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char *aux;
+
+	aux = *a;
+	*a = *b;
+	*b = aux;
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+void	bubbleSort(char **arr, int n) 
+{ 
+	int i;
+	int j;
+
+	i = 0;
+	while (i < n - 1)
+	{
+		j = 0;
+		while (j < n - i - 1)
+		{
+			if (ft_strcmp(arr[j], arr[j + 1]) > 0)
+			{
+				ft_swap(&arr[j], &arr[j + 1]);
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int i;
+
+	i = 0;
+	while (i < argc - 1)
+	{
+		ft_swap(&argv[i + 1], &argv[i]);
+		i++;
+	}
+	argc--;
+	bubbleSort(argv, argc);
+	i = 0;
+	while (i < argc){
+		ft_ptint_str(argv[i]);
+		i++;
+	}
+}
