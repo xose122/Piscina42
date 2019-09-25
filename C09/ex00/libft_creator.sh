0a2a595e0ab7ecbee01f839493daf0cb5d13d7d6
Jorge Gomez bravo <jgomez-b@c2r10s3.42madrid.com>
2019-09-25 17:53:57 +0200

    C09 hasta ex02

---
+++ C09/ex00/libft_creator.sh
@@ -0,0 +1,15 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    libft_creator.sh                                   :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: jgomez-b <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2019/09/25 16:48:14 by jgomez-b          #+#    #+#              #
+#    Updated: 2019/09/25 16:48:15 by jgomez-b         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+gcc -Wall -Wextra -Werror -c ft_putchar.c ft_swap.c ft_putstr.c ft_strlen.c ft_strcmp.c
+ar rc libft.a ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
+rm *.o
